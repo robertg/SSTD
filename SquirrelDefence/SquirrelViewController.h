3ff8296a74aa66a522c5214379a653632476b7d0
Park Sunwoo <parksunwoo@v1020-wn-243-178.campus-dynamic.uwaterloo.ca>
2013-11-09 18:44:06 -0500

    First Push

---
+++ SquirrelDefence/SquirrelViewController.h
@@ -0,0 +1,14 @@
+//
+//  SquirrelViewController.h
+//  SquirrelDefence
+//
+
+//  Copyright (c) 2013 Sinusoidal Squirrels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface SquirrelViewController : UIViewController
+
+@end
---
+++ SquirrelDefence/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SquirrelDefence
+//
+//  Created by Park Sunwoo on 11/9/2013.
+//  Copyright (c) 2013 Sinusoidal Squirrels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SquirrelAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SquirrelAppDelegate class]));
+    }
+}
