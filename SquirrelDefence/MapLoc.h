1f7096b082b2ba5eed729d6f46ca2a5f8d05ba88
Robert Gawdzik <rgawdzik@hotmail.com>
2013-11-09 21:16:21 -0500

    Added a MapLoc object, working on generatePath(). Renamed SquirrelMyScene to MapScene

---
+++ SquirrelDefence/MapLoc.h
@@ -0,0 +1,19 @@
+//
+//  MapLoc.h
+//  SquirrelDefence
+//
+//  Created by Robert Gawdzik on 11/9/2013.
+//  Copyright (c) 2013 Sinusoidal Squirrels. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapLoc : NSObject
+
+@property int X;
+@property int Y;
+
+-(id)initX: (int)X
+     initY: (int)Y;
+
+@end
